f0adf0dd0c2e850b9621efdbd7c512cda14ccc97
gowtham207 <71398314+gowtham207@users.noreply.github.com>
2022-06-15 19:25:13 +0530

    Update firewall.py

--- firewall.py
+++ firewall.py
@@ -3,16 +3,32 @@ import scapy.all as scapy
 import netfilterqueue
 import user_input
 
-ip = "192.168.2.129"
-block = "10.0.2.1"
-tcp_port = [22]
-udp_port = [22]
+ip = user_input.enter_ip()
+block = user_input.ban_ip()
+tcp_port = user_input.ban_tcp()
+udp_port = user_input.ban_udp()
+
+def report(val):
+    a = open("report.txt","a")
+    if len(val) == 1:
+        c = "\n block ip  from "+val[0]
+        a.write(c)
+    
+    else:
+        c = "\n request from \nip: "+val[0]+"\nport:"+val[1]
+        a.write(c)
+    
+
+
+
 def firewall(pkt):
     #print(pkt)
     sca = scapy.IP(pkt.get_payload())
     if sca.haslayer(scapy.ICMP):
         if sca[scapy.ICMP].type == 8 and sca.dst == ip:
             print("request from "+str(sca.src))  
+            c = [str(sca.src)]
+            report(c)
             pkt.drop()
             return
         else:
@@ -22,39 +38,36 @@ def firewall(pkt):
     
     if sca.src in block:
         print("blocking the ip"+str(sca.src))
+        c = [str(sca.src)]
+        report(c)
         pkt.drop()
         return 
    
     if sca.haslayer(scapy.TCP):
-        if sca[scapy.TCP].dport in tcp_port:
+        if sca[scapy.TCP].dport in tcp_port and sca[scapy.IP].src!=ip:
+            #print(sca.show())
             port = sca[scapy.TCP].dport
             ip_add = sca.src
-            print("port "+str(port)+" accessed by "+str(ip_add))
+            print("port "+str(port)+"  TCP accessed by "+str(ip_add))
+            c = [str(ip_add),str(port)]
+            report(c)
             pkt.drop()
             return
 
     if sca.haslayer(scapy.UDP):
-        if sca[scapy.UDP].dport in udp_port:
+        if sca[scapy.UDP].dport in udp_port and sca[scapy.IP].src!=ip:
+            
             port = sca[scapy.UDP].dport
             ip_add = sca.src
             print("port "+str(port)+" accessed by "+str(ip_add))
+            c = [str(ip_add),str(port)]
+            report(c)
             pkt.drop()
             return 
 
     #print(sca.show())  
     pkt.accept()
-
-
-
-
-      
-
-    
-    
-    
-    
-
-
+   
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(1,firewall)
 queue.run()
